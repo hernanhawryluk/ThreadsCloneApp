ce45aaa48e4c76f4df098d15a0aec6ba95e9c41f
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-08-20 12:39:09 -0300

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Hernan Hawryluk on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
