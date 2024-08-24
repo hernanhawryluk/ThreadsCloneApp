a774729f2ea572d21fcacc75f3376e3c92828807
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-08-24 11:59:33 -0300

    Implemented fetching of posts

---
+++ ThreadsClone/Core/CreateThread/ViewModel/CreateThreadViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CreateThreadViewModel.swift
+//  ThreadsClone
+//
+//  Created by Hernan Hawryluk on 24/08/2024.
+//
+
+import SwiftUI
+
+struct CreateThreadViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateThreadViewModel()
+}
---
+++ ThreadsClone/Core/Feed/ViewModel/FeedViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FeedViewModel.swift
+//  ThreadsClone
+//
+//  Created by Hernan Hawryluk on 24/08/2024.
+//
+
+import Foundation
