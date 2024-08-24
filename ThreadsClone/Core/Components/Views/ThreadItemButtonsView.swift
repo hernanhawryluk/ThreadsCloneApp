ef9eedd219c6a0537114397634b9d7d246e02c09
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-08-24 16:45:10 -0300

    Added likes service and implemented it in views for better interaction

---
+++ ThreadsClone/Core/Components/Views/ThreadItemButtonsView.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadItemButtonsView.swift
+//  ThreadsClone
+//
+//  Created by Hernan Hawryluk on 24/08/2024.
+//
+
+import SwiftUI
+
+struct ThreadItemButtonsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreadItemButtonsView()
+}
---
+++ ThreadsClone/Services/LikeService.swift
@@ -0,0 +1,8 @@
+//
+//  LikeService.swift
+//  ThreadsClone
+//
+//  Created by Hernan Hawryluk on 24/08/2024.
+//
+
+import Foundation
