d5ddbe76bb99278c06075ca1c3608e29f75bef73
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-08-25 18:58:40 -0300

    Added reply service for uploading replies and implemented a Toast to display the upload progress.

---
+++ ThreadsClone/Core/Feed/ViewModel/ReplyThreadViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ReplyThreadViewModel.swift
+//  ThreadsClone
+//
+//  Created by Hernan Hawryluk on 25/08/2024.
+//
+
+import SwiftUI
+
+struct ReplyThreadViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReplyThreadViewModel()
+}
---
+++ ThreadsClone/Model/Reply.swift
@@ -0,0 +1,8 @@
+//
+//  Reply.swift
+//  ThreadsClone
+//
+//  Created by Hernan Hawryluk on 25/08/2024.
+//
+
+import Foundation
